b93d0171daa938987db9f43c64a534ee0c801ad7
Roger Gómez <gomez.roger779@gmail.com>
2020-05-31 11:54:55 -0400

    Removed useState from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { Helmet } from "react-helmet"
 import Home from '../components/home'
 import About from '../components/about'
