db0a9e7341042e2fdfd034174a521c26864a832f
Roger Gomez <gomez.roger779@gmail.com>
2023-11-02 11:54:57 -0400

    Updated about me text

--- src/components/about.js
+++ src/components/about.js
@@ -57,12 +57,12 @@ export default () => (
       </ImageSection>
       <TextDiv>
         <AboutText>
-          24 year-old software developer. I'm an avid tech enthusiast, trying to
+          25 year-old software developer. I'm an avid tech enthusiast, trying to
           keep in touch with current and emerging technologies and aiming to
           learn and improve my skills every day.
         </AboutText>
         <AboutText>
-          Currently i'm working as a react developer on Vascar Solutions, where
+          Currently I'm working as a react developer at Vascar Solutions, where
           I'm part of the development team. I like teamwork and constantly
           learning from my co-workers.
         </AboutText>
